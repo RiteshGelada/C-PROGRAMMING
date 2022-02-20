e845c4f3356bbd905266a937cddcb7b3959f3ce7
RiteshGelada <94109372+RiteshGelada@users.noreply.github.com>
2022-02-20 11:21:29 +0530

    Add files via upload

---
+++ day3_2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+   int num = 25; // integer variable 
+   float f = 2.6; // Float variable
+   char ch = 'A'; // character variable
+
+   printf("integer value = %d \n float value = %f \n character value = %c",num,f,ch);
+
+ return 0;
+}   
