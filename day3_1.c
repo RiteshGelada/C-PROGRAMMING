02a8be25a546bd211571a73e278465e2f4bf5482
RiteshGelada <94109372+RiteshGelada@users.noreply.github.com>
2022-02-20 11:21:14 +0530

    Add files via upload

---
+++ day3_1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+// Vaue = 40
+// Num1 = 20 Num2 = 10
+
+int main(void)
+{
+  int v1 = 40;
+  int num1 = 20;
+  int num2 = 10;
+   
+   printf("\nValue = %d",v1);
+   printf("\nnum1 = %d",num1);
+   printf("\nnum2 = %d",num2);
+
+  return 0;
+}  
