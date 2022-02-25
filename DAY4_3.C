dd3948a50b8287408dca7f5a83c0cfe73d0f825d
RiteshGelada <94109372+RiteshGelada@users.noreply.github.com>
2022-02-25 20:05:34 +0530

    Add files via upload

---
+++ DAY4_3.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(void)
+{
+  float f_val=8.43;
+  printf("%f",f_val);
+  printf("\n%9.2f",f_val);
+
+
+
+
+
+
+
+    return 0;
+}
+/*
+int main(void)
+{
+    int num1=45;
+    int num2=500;
+    int num3=4000;
+    int num4=500;
+    int num5=56;
+    int num6=6000;
+    printf("",num1,num2,num3,);
+    printf("",num4,num5,num6);
+    printf("%9d %9d %9d",num1,num2,num3);
+    printf("\n%-9d %-9d %-9d",num4,num5,num6);
+     
+
+     return 0;
+}
+*/
