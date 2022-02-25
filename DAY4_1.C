cebcacce003c3e49e8154503c79958dfd8aa03fe
RiteshGelada <94109372+RiteshGelada@users.noreply.github.com>
2022-02-25 20:04:40 +0530

    Add files via upload

---
+++ DAY4_1.C
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+/*
+int main(void)
+{
+  int a;
+  a=20,5,7;
+  //if any variable is assigned with multiple number of values,
+  //without brackets then compiler assigns leftmot value to that variable
+  printf("a =%d",a);
+
+
+  return 0;
+}
+
+/*
+int main(void)
+{
+   int a=(10,4,2,7);
+   //if any vaiable given multiple values inside the bracket,
+   //then rightmost value will be assigned to variable
+   printf("a=%d",a);
+
+
+
+  return 0;
+}
+/*
+int main(void)
+{
+  int i =5;
+  float f =4.3;
+  char ch ='A';
+  double d =9.5;
+ printf("size = %d",sizeof(i)+sizeof(f));
+ printf("\n size = %d",sizeof(i)+sizeof(f)+sizeof(ch));
+ printf("\n size = %d",sizeof(i+f));
+ printf("\n size = %d",sizeof(char)+sizeof(float)+sizeof(int));
+ printf("\n display size = %d",sizeof(i+f+ch+d));
+ //largest data type will take its size i.e d = 8 bytes
+  return 0;
+}
+*/
