6ad9a7092244be12a20d92e9755bcce2b76563bd
RiteshGelada <94109372+RiteshGelada@users.noreply.github.com>
2022-02-25 20:05:08 +0530

    Add files via upload

---
+++ DAY4_2.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(void)
+
+{
+   int num=0x45;
+   //any numberpreceded with 0x or ox is cosidered as hexa decimal 
+   printf("num =%d num=%x num =%o",num,num,num);
+
+
+
+  return 0 ;
+}
+
+/*
+int main()
+{
+  //%o = octal number representation
+  //%x = hexadecimal number representation
+  int num=064;
+  // whenever a number is preceded with o or 0 is considered as octal number 
+  printf("Num=%d Num=%x Num=%o",num,num,num);
+
+
+
+  return 0;
+}
+*/
