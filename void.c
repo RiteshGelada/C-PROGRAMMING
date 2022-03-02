56b5b67e03bf120af7139627ae2abf3f2f254ef5
RiteshGelada <94109372+RiteshGelada@users.noreply.github.com>
2022-03-02 19:33:36 +0530

    Add files via upload

---
+++ void.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+//#define NULL (void*)0
+int main()
+{
+    
+   int *p=NULL; 
+   int *q;
+   int *r;
+   int *s;
+   printf("%d \n",q);
+   printf("%d \n",r);
+   printf("%d \n",s);
+    return 0;
+}
